883fe008fe423fdc7eb81ec244d989a798ff573b
thefastet <137315122+thefastet@users.noreply.github.com>
2023-07-29 14:55:10 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -19,7 +19,7 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=28019212&pid=acc56ebe038246e082b2519d11bcaeb9&tid=15a7fe4257d207ba18b8473613a285d9",
+    "https://www.maimemo.com/share/page?uid=28019212&pid=6dc9bf4ffde7a017a5c8659c5f17929e&tid=1a6cce4afad46c8e1020b0ca65ff2179",
   ]
   /**
    * 生成随机数字
